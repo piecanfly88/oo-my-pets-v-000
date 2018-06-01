10564a80513d1a4371d1f4d24bc9826127bf444e
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-01 07:40:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
-  # code goes here
+  @@all = []
+
+  attr_reader :species
+
+  def initialize(species)
+    @species = species
+    @@all << self
+  end
+
+  def self.reset_all
+    @@all.clear
+    # @@all.each{|owner| }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
 end
