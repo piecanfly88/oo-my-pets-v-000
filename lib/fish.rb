669bf3a23ca411e77b52b60a947e7b1c30ff391d
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-01 09:33:40 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -2,8 +2,8 @@ class Fish
   attr_accessor :mood
   attr_reader :name
 
-  def initialize(name, mood = "nervous")
+  def initialize(name)
     @name = name
-    @mood = mood
+    @mood = "nervous"
   end
 end
